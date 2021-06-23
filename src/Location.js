1a711f6c217f5d0821bc8f0072e03e654482eba2
Mike Smith (Office) <mike@sourcewebdev.co.uk>
2021-06-23 17:55:09 +0100

    Revert "create next app"
    This reverts commit 067962d3e4fac58634471b4cc79ab5f229c7128b.

---
+++ src/Location.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+export class Location extends React.Component {
+  render() {
+    return (
+      <div className="location">
+        <span>You are at:</span>
+        <span>{this.props.latitude}</span>
+        <span>{this.props.longitude}</span>
+      </div>
+    )
+  }
+}
