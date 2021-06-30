f7645e9442a1c06ed7b8c18227a95ee7240a1dbe
Mike Smith (Office) <mike@sourcewebdev.co.uk>
2021-06-30 17:21:54 +0100

    get coords using useGeolocation() hook from ‘react-use’ lib

--- components/LocationForecast.js
+++ components/LocationForecast.js
@@ -1,45 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useGeolocation } from 'react-use';
 import { Location } from './Location.js';
 import { Weather } from './Weather.js';
 
 const LocationForecast = () => {
-  // default state is to locate user on null island
-  // https://en.wikipedia.org/wiki/Null_Island
-  const [coords, setCoords] = useState({ lat: 0, long: 0, geoip: false });
+  // TO DO: Find a better way to get location via react hook as this is v.buggy
+  const position = useGeolocation();
 
-  // first find out where they are:
-  useEffect(() => {
-    const getLocation = async () => {
-      if ('geolocation' in navigator) {
-        // browser geolocation available, use it:
-        navigator.geolocation.getCurrentPosition((position) => {
-          setCoords({
-            lat: position.coords.latitude,
-            long: position.coords.longitude,
-            geoip: false,
-          });
-        });
-      } else {
-        // browser geolocation NOT available, use freegeoip.app:
-        const response = await fetch(`https://freegeoip.app/json/`).then(
-          (response) => response.json()
-        );
-        setCoords({
-          lat: response.latitude,
-          long: response.longitude,
-          geoip: true,
-        });
-      }
-    };
-    getLocation();
-  }, []);
-
-  //return <div>{JSON.stringify(coords)}</div>;
+  console.log(position);
 
   return (
     <>
-      {coords.geoip && <h2>Used GEOIP</h2>}
-      <Location latitude={coords.lat} longitude={coords.long}></Location>
+      <Location
+        latitude={position.latitude}
+        longitude={position.longitude}
+      ></Location>
     </>
   );
 };
